ce48654eec200bbeb131a37d82e2d4dcb3465720
0xor1 <daniel.robinson.open@gmail.com>
2014-08-01 21:29:53 +0100

    add in relevant url

--- lib/model/sandbox_purity_model.dart
+++ lib/model/sandbox_purity_model.dart
@@ -4,6 +4,7 @@
 
 library sandbox.purity.model;
 
+import 'dart:io' as io;
 import 'package:sandbox_purity/interface/sandbox_purity_interface.dart';
 import 'package:purity/purity.dart';
 
@@ -14,7 +15,21 @@ class GoogleLogin extends Source implements IGoogleLogin{
   }
 
   void login(){
-    emitEvent(new OAuth2LoginUrlRedirection()..url = 'https://accounts.google.com/o/oauth2/auth?redirect_uri=https%3A%2F%2Fdevelopers.google.com%2Foauthplayground&response_type=code&client_id=407408718192.apps.googleusercontent.com&scope=https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fuserinfo.profile&approval_prompt=force&access_type=offline');
+
+    var uri = new Uri.https('accounts.google.com', '/o/oauth2/auth', {
+      'redirect_uri' : 'http://localhost:4346/oauth2redirect',
+      'response_type' : 'code',
+      'client_id' : '1084058127345-j5m0ra24902vhuggtf7h9pj27itnr8cl.apps.googleusercontent.com',
+      'scope' : 'https://www.googleapis.com/auth/userinfo.profile https://www.googleapis.com/auth/userinfo.email',
+      'approval_prompt' : 'auto',
+      'access_type' : 'offline',
+      'state' : 'hellojusttesting'
+    });
+
+    print(uri);
+
+    emitEvent(new OAuth2LoginUrlRedirection()..url = uri.toString());
+
   }
 
 }
