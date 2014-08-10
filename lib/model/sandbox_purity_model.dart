381fa91f0bd7e5bb1383d4cf4e146e0379179680
0xor1 <daniel.robinson.open@gmail.com>
2014-08-10 21:46:24 +0100

    first working google login

--- lib/model/sandbox_purity_model.dart
+++ lib/model/sandbox_purity_model.dart
@@ -6,6 +6,7 @@ library sandbox.purity.model;
 
 import 'dart:io';
 import 'dart:async';
+import 'package:http/http.dart' as http;
 import 'dart:convert' as convert;
 import 'package:bson/bson.dart';
 import 'package:sandbox_purity/interface/sandbox_purity_interface.dart';
@@ -59,7 +60,13 @@ class GoogleLogin extends Source implements IGoogleLogin{
 
   static void _handleOauth2Redirect(HttpRequest authReq) {
 
-    var loginId = new ObjectId.fromHexString(authReq.uri.queryParameters['state']);
+    var loginIdHexString = authReq.uri.queryParameters['state'];
+
+    if(loginIdHexString == null){
+      return;
+    }
+
+    var loginId = new ObjectId.fromHexString(loginIdHexString);
     var login = _activeLogins.remove(loginId);
 
     if(login != null){
@@ -70,30 +77,26 @@ class GoogleLogin extends Source implements IGoogleLogin{
       else{
         //swap authorization token for access token
         var code = authReq.uri.queryParameters['code'];
-        var uri = new Uri.https('accounts.google.com', '/o/oauth2/token', {
+        var uri = new Uri.https('accounts.google.com', '/o/oauth2/token');
+        var fields = {
           'redirect_uri' : 'http://localhost:4347',
           'code' : code,
           'client_id' : '1084058127345-j5m0ra24902vhuggtf7h9pj27itnr8cl.apps.googleusercontent.com',
           'client_secret' : 'yX9U5oCY3G4UE9rQQ_1gDC_B',
           'grant_type' : 'authorization_code'
-        });
+        };
         var urlStr = uri.toString();
-        var httpClient = new HttpClient();
-        httpClient.postUrl(uri)
-        .then((HttpClientRequest accessReq) => accessReq.close())
-        .then((HttpClientResponse accessRes) => convert.UTF8.decodeStream(accessRes))
-        .then((String accessJson){
+        var httpClient = new http.Client();
+        httpClient.post(uri, body: fields)
+        .then((http.Response accessRes){
           //get user info
-          Map<String, String> accessObj = convert.JSON.decode(accessJson);
+          Map<String, String> accessObj = convert.JSON.decode(accessRes.body);
           var accessToken = accessObj['access_token'];
-          var uri = new Uri.https('googleapis.com', '/userinfo/v2/me');
-          httpClient.postUrl(uri)
-          .then((HttpClientRequest dataReq){
-            dataReq.headers.add('Authorization', 'Bearer $accessToken');
-            return dataReq.close();
-          }).then((HttpClientResponse dataRes) => convert.UTF8.decodeStream(dataRes))
-          .then((String dataRes){
-            Map<String, String> dataObj = convert.JSON.decode(dataRes);
+          var uri = new Uri.https('www.googleapis.com', '/userinfo/v2/me');
+          httpClient.get(uri, headers: {'Authorization': 'Bearer $accessToken'})
+          .then((http.Response dataRes){
+            var str = dataRes.body;
+            Map<String, String> dataObj = convert.JSON.decode(dataRes.body);
             login.emitEvent(
               new OAuth2LoginUserDetails()
               ..firstName = dataObj['given_name']
